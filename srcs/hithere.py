854c2e85d2b4255f1fdb316bfb112f5814526fdb
Joep Barmentlo <jbarment@student.42.fr>
2021-08-09 21:26:57 +0200

    line detection better

--- srcs/hithere.py
+++ srcs/hithere.py
@@ -4,6 +4,8 @@ import cv2
 import depthai as dai
 import math
 import numpy as np
+from checkerboard import detect_checkerboard
+
 
 # Create pipeline
 pipeline = dai.Pipeline()
@@ -58,26 +60,36 @@ with dai.Device(pipeline) as device:
 	while(True):
 		edgeRgb = edgeRgbQueue.get()
 		edgeRgbFrame = edgeRgb.getFrame()
-		edgeRgbFrame[edgeRgbFrame < 150] = 0
+		filterEdgeFrame = np.copy(edgeRgbFrame)
+		filterEdgeFrame[filterEdgeFrame < 150] = 0
+		# edgeRgbFrame[edgeRgbFrame < 150] = 0
 		videoFrame = videoQueue.get().getCvFrame()
 		cv2.imshow(edgeRgbStr, edgeRgbFrame)
 		# print(f"vid shape: {videoFrame.shape}. EdgeShape: {edgeRgbFrame.shape}")
 		# print(type(edgeRgbFrame))
 		
+	
 
 		# Show the frame
 
 		# LineFrame = cv2.cvtColor(edgeRgbFrame, cv2.COLOR_GRAY2BGR)
 		# LineFrame = np.copy(edgeRgbFrame)
-		lines = cv2.HoughLinesP(edgeRgbFrame, 1, np.pi/180, 100, 500, 0)
+		lines = cv2.HoughLinesP(filterEdgeFrame, 1, np.pi/180, threshold = 100, minLineLength=250, maxLineGap=50)
 		if lines is not None:
 			print(len(lines))
 			for i in range(0, len(lines)):
 				l = lines[i][0]
 				cv2.line(videoFrame, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv2.LINE_AA)
-		cv2.imshow(videoStr, videoFrame)
+		cv2.imshow("HoughlinesP edge", videoFrame)
 
-		# lines = cv2.HoughLines(edgeRgbFrame, 10, np.pi / 180, 10000, None, 0, 0)
+		# ret, corners = cv2.findChessboardCorners(videoFrame, (7,7))
+		# print(f"ret: {ret}, corners:\n{corners}")
+		# cv2.drawChessboardCorners(videoFrame, (7,7), corners, ret)
+
+		# if (ret):
+		# 	cv2.imshow("ChessBoardCorners", videoFrame)
+
+		# lines = cv2.HoughLines(edgeRgbFrame, 1, np.pi / 180, 200)
     
 		# if lines is not None:
 		# 	print(len(lines))
@@ -88,11 +100,12 @@ with dai.Device(pipeline) as device:
 		# 		b = math.sin(theta)
 		# 		x0 = a * rho
 		# 		y0 = b * rho
-		# 		pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-		# 		pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-		# 		cv2.line(LineFrame, pt1, pt2, (0,0,255), 1, cv2.LINE_AA)
-		
-		# cv2.imshow("Lines", LineFrame)
+		# 		pt1 = (int(x0 + 10000*(-b)), int(y0 + 10000*(a)))
+		# 		pt2 = (int(x0 - 10000*(-b)), int(y0 - 10000*(a)))
+		# 		cv2.line(videoFrame, pt1, pt2, (0,0,255), 1, cv2.LINE_AA)
+
+		# cv2.imshow("Corners", videoFrame)
+		# cv2.cvtColor(videoFrame, cv2.COLOR_RGB2GRAY)
 		# print("MAMARASEASE\n")
 
 		key = cv2.waitKey(1)
